980d75d46ec152f07de3dba34bafb868e5bc0808
pierpaolo1989 <pierpaolo.pdd@gmail.com>
2025-06-27 08:11:05 +0200

    Added main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,187 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(JavettieriApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class JavettieriApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Javettieri',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class Lesson {
   final String title;
+  final String content;
+  bool isRead;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Lesson({required this.title, required this.content, this.isRead = false});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
-  void _incrementCounter() {
+class _HomeScreenState extends State<HomeScreen> {
+  List<Lesson> lessons = [
+    Lesson(title: 'Variabili in Java', content: 'Contenuto su variabili...'),
+    Lesson(title: 'Cicli for e while', content: 'Contenuto sui cicli...'),
+    Lesson(title: 'Classi e Oggetti', content: 'Contenuto su OOP...'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Javettieri')),
+      body: ListView.builder(
+        itemCount: lessons.length,
+        itemBuilder: (context, index) {
+          final lesson = lessons[index];
+          return ListTile(
+            title: Text(lesson.title),
+            subtitle: Text(lesson.isRead ? 'Letta' : 'Da leggere'),
+            trailing: Icon(
+              lesson.isRead ? Icons.check_box : Icons.check_box_outline_blank,
+              color: lesson.isRead ? Colors.green : null,
+            ),
+            onTap: () async {
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LessonScreen(lesson: lesson),
+                ),
+              );
+              if (result != null) {
+                setState(() {
+                  lessons[index].isRead = result;
+                });
+              }
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.quiz),
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => QuizScreen()),
+        ),
+      ),
+    );
+  }
+}
+
+class LessonScreen extends StatefulWidget {
+  final Lesson lesson;
+
+  LessonScreen({required this.lesson});
+
+  @override
+  _LessonScreenState createState() => _LessonScreenState();
+}
+
+class _LessonScreenState extends State<LessonScreen> {
+  late bool isRead;
+
+  @override
+  void initState() {
+    super.initState();
+    isRead = widget.lesson.isRead;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.lesson.title)),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(widget.lesson.content),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Checkbox(
+                  value: isRead,
+                  onChanged: (val) {
+                    setState(() => isRead = val ?? false);
+                  },
+                ),
+                Text('Segna come letta'),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context, isRead),
+              child: Text('Salva'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class QuizScreen extends StatefulWidget {
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  int questionIndex = 0;
+  int score = 0;
+  List<Map<String, Object>> questions = [
+    {
+      'question': 'Qual è il tipo di dato per i numeri interi?',
+      'options': ['String', 'int', 'double', 'boolean'],
+      'answer': 'int'
+    },
+    {
+      'question': 'Cosa stampa System.out.println("Hello");?',
+      'options': ['Hello', 'System.out.println', 'Errore', 'Console'],
+      'answer': 'Hello'
+    },
+  ];
+
+  void answerQuestion(String selected) {
+    final correct = questions[questionIndex]['answer'];
+    if (selected == correct) score++;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      questionIndex++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: Text('Quiz Javettieri')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: questionIndex < questions.length
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(questions[questionIndex]['question'] as String,
+                      style: TextStyle(fontSize: 20)),
+                  ...(questions[questionIndex]['options'] as List<String>).map(
+                    (option) => ElevatedButton(
+                      onPressed: () => answerQuestion(option),
+                      child: Text(option),
+                    ),
+                  ),
+                ],
+              )
+            : Center(
+                child: Text('Quiz finito! Punteggio: $score',
+                    style: TextStyle(fontSize: 24)),
+              ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
